3dfa4fd954174dca0610e04a5251c0e9c5f72ae7
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2021-11-16 15:03:20 -0300

    tailwid v1.0

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        spacing: {
+        '13': '3.25rem',
+        '15': '3.75rem',
+        '128': '32rem',
+        '144': '36rem',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+
+}
